80e34b6c028ee9dcb4cf90d84eaee0c31dde31d6
thisi <thisi@DESKTOP-58C15JB>
2019-02-10 00:25:39 +0530

    Enabled Swagger UI.

--- student-admin/src/main/java/com/example/student/StudentAdminApplication.java
+++ student-admin/src/main/java/com/example/student/StudentAdminApplication.java
@@ -7,15 +7,26 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jms.annotation.EnableJms;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableCaching
 @EnableJms
+@EnableSwagger2
 @ComponentScan(basePackages = { "com.example.student" })
 public class StudentAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentAdminApplication.class, args);
+	}
+
+	public Docket studentApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.student.rest.controller")).build();
 	}
 
 }
