b2503a720568f41c3fae766e155f66505b68991f
Praveen Kumaravel <praveen1994kp@gmail.com>
2020-08-16 17:51:41 +0530

    SQL - DDL commands

---
+++ fitnesspartner-user/sql/fitnesspartner-ddl.sql
@@ -0,0 +1,25 @@
+CREATE TABLE fitnesspartner.`user` (
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NULL,
+	weight_in_kg INT NULL,
+	height_in_cm INT NULL,
+	bmi BIGINT NULL,
+	email varchar(100) NOT NULL,
+	password varchar(100) NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY (email)
+)
+
+
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci
+COMMENT='User Details';
+
+CREATE USER 'springuser'@'%';
+ALTER USER 'springuser'@'%'
+IDENTIFIED BY 'springuser' ;
+
+GRANT Update ON fitnesspartner.* TO 'springuser'@'%';
+GRANT Select ON fitnesspartner.* TO 'springuser'@'%';
+GRANT Insert ON fitnesspartner.* TO 'springuser'@'%';
+GRANT Delete ON fitnesspartner.* TO 'springuser'@'%';
